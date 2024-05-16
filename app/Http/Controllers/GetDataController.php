60dd487c04501427e1257d5676dcffcc8494682c
ITSensync <it.sensync@gmail.com>
2024-05-16 14:06:42 +0700

    d

--- app/Http/Controllers/GetDataController.php
+++ app/Http/Controllers/GetDataController.php
@@ -203,7 +203,7 @@ class GetDataController extends Controller
     }
 
 
-    public function getWeeklyDataById($id)
+    public function getWeeklyDataById ($id) 
     {
         $validIds = [
             'sparing01' => 'Gistex',
@@ -221,46 +221,36 @@ class GetDataController extends Controller
             'weaving02' => 'weaving02',
             'spinning' => 'spinning',
         ];
-    
+
         if (!array_key_exists($id, $validIds)) {
             return response()->json([
                 'status' => 'Error',
                 'message' => 'Invalid ID Provider'
             ], 400);
         }
-    
-        $title = $validIds[$id];
-    
+
+
+            $title = $validIds[$id];
         $query = "
-        SELECT 
-            year_week,
-            MIN(start_date) AS start_date,
-            MAX(end_date) AS end_date,
-            COUNT(*) AS total_records
-        FROM (
             SELECT 
-                YEARWEEK(time, 1) AS year_week,
+                WEEK(time, 1) - WEEK(DATE_SUB(time, INTERVAL DAYOFMONTH(time) - 1 DAY), 1) + 1 AS week_in_month,
                 MIN(time) AS start_date,
-                MAX(time) AS end_date
+                MAX(time) AS end_date,
+                COUNT(*) AS total_records
             FROM $id
-            WHERE 
-                (YEAR(time) = 2024 AND MONTH(time) = 5) OR
-                (YEAR(time) > 2024 AND MONTH(time) >= 5) 
-                AND WEEKDAY(time) BETWEEN 0 AND 6
-            GROUP BY YEARWEEK(time, 1) /* Menambahkan year_week ke dalam GROUP BY */
-        ) AS subquery
-        GROUP BY year_week /* Mengelompokkan berdasarkan year_week di luar subquery */
-        ORDER BY year_week;
+            WHERE MONTH(time) = MONTH(CURDATE()) AND YEAR(time) = YEAR(CURDATE())
+            GROUP BY week_in_month
+            ORDER BY week_in_month;
         ";
-    
+
         $results = DB::select(DB::raw($query));
-    
+
         // Format output JSON
         $data = [];
         foreach ($results as $result) {
             $interval_date = date('d/m/Y', strtotime($result->start_date)) . ' - ' . date('d/m/Y', strtotime($result->end_date));
             $data_count = $result->total_records;
-    
+            
             // Hitung persentase
             $expected_count = 5040;
             $percent = ($data_count / $expected_count) * 100;
@@ -268,23 +258,20 @@ class GetDataController extends Controller
                 $percent = 100;
             }
             $percent = number_format($percent, 2);
-    
+
             $data[] = [
                 'interval_date' => $interval_date,
                 'data_count' => $data_count,
                 'percent' => $percent
             ];
         }
-    
+
         return response()->json([
             'status' => 'OK',
             'message' => 'Success',
             'id' => $id,
-            'title' => $title,
+            'title' => $title, // Sesuaikan dengan nilai yang sesuai
             'data' => $data
         ]);
     }
-    
-    
-    
 }
