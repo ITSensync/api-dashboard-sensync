ce622ddb6d920c22b9f191a7d319ab229dcef57a
ITSensync <it.sensync@gmail.com>
2024-06-13 10:28:41 +0700

    u

--- routes/api.php
+++ routes/api.php
@@ -45,4 +45,4 @@ use App\Http\Controllers\PreviousMonthDataController;
 
 // router histroy data per 2 menit
     Route::get('sparing/previous-month-data/{id}/{month}/{year}', [PreviousMonthDataController::class, 'getPreviousMonthData']);
-    Route::get('sparing/percentages/{month}/{year}', [PreviousMonthDataController::class, 'getPreviousMonthData']);
+    Route::get('sparing/percentages/{month}/{year}', [PreviousMonthDataController::class, 'getPreviousAveragePercentages']);
